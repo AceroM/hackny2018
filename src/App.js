c851be43c6fa43e59d56838fe6824f849ce412a8
Miguel Acero <miguel@Miguels-MacBook-Pro.local>
2018-09-29 18:17:14 -0400

    something works lol

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  this.state = {
+    img : ""
+  }
+
+  componentDidMount(){
+    fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDyHZ3-o29xV9mjOmXwUFLvkyar4RsoSvU', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "requests":[
+          {
+            "image":{
+              "content": {this.state}
+            },
+            "features":[
+              {
+                "type":"TEXT_DETECTION",
+                "maxResults":2
+              }
+            ]
+          }
+        ]
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
   render() {
     return (
       <div className="App">
